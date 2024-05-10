13f8459b4152135bba40f2e98b418b5e65c1f610
SksOp <shubhaman47@gmail.com>
2024-05-11 04:12:44 +0530

    add altcha-widget custom jsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import Image from "next/image";
 
-import { useEffect, useState } from "react";
 import { AltchaContextProvider } from "@/context/altcha/provider";
+
 export default function Home() {
   return (
     <AltchaContextProvider>hello you are now verified</AltchaContextProvider>
---
+++ context/altcha/altcha-widget.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "altcha-widget": any;
+    }
+  }
+}
+
+export {};
